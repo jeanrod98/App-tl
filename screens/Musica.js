9c4b5fa0c4e946112bb8bcefc4dc6a19fb09e8b3
jeanrod28 <jcrod.jc@gmail.com>
2024-06-03 12:36:47 -0500

    terminado modulo de musica

--- screens/Musica.js
+++ screens/Musica.js
@@ -1,43 +1,106 @@
 import {
-    View,
-    Text,
-    Dimensions,
-    StyleSheet,
-    TouchableOpacity,
-  } from "react-native";
-  import { AntDesign } from "@expo/vector-icons";
-  import useAuth from "../Hooks/useAuth";
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import useAuth from "../Hooks/useAuth";
+import { useEffect, useState } from "react";
+
+import musica_fondo_1 from "../assets/musica_fondo_1.jpg";
+import ColoresElegirObjetos from "../components/ColoresElegirObjetos";
+import EscogerInstrumento from "../components/EscogerInstrumento";
   
   const Musica = () => {
-    const { dataAlert, setDataAlert, logOut, setOption, option } = useAuth();
   
+
+    const { dataAlert, setDataAlert, logOut, setOption, option } = useAuth();
+    
+    const [ escogerObjetos, setEscogerObjetos ] = useState(false);
+    const [ escogerNumeros, setEscogerNumeros ] = useState(false);
+    const [ encontrarNumeros, setEncontrarNumeros ] = useState(false);
+
+
+    useEffect(() => {
+
+      //Obtener tiempo que lleva en esta opcion
+      obtenerTiempo();
+    }, [])
+  
+    const obtenerTiempo = () => {
+      console.log(12345);
+    }
+
     return (
-      <View style={styles.containerMusica}>
-        <View style={styles.header}>
-          <View style={styles.opcionesModulo}>
-            <Text>Opcion 1</Text>
-            <Text>Opcion 2</Text>
-            <Text>Opcion 3</Text>
-          </View>
+      <View style={styles.containerColores}>
+      <View style={styles.header}>
+        
+        <TouchableOpacity
+          onPress={() => {
+            setOption({...option,
+              next: false,
+            });
+          }}
+        >
+          <AntDesign
+            style={styles.btnClose}
+            name="closecircle"
+            size={28}
+            color="red"
+          />
+        </TouchableOpacity>
+      </View>
+      <ImageBackground source={musica_fondo_1} resizeMode="cover"  imageStyle={{ opacity: 1, }} style={styles.contenido}>
+      <View style={styles.opcionesModulo}>
           <TouchableOpacity
+            style={styles.botonSubMenu}
             onPress={() => {
-              setOption({...option,
-                next: false,
-              });
+              setEscogerNumeros(false);
+              setEncontrarNumeros(false);
+              setEscogerObjetos(true);
             }}
           >
-            <AntDesign
-              style={styles.btnClose}
-              name="closecircle"
-              size={28}
-              color="red"
-            />
+            <Text style={styles.txtSubmenu}>Escoge el instrumento musical</Text>
           </TouchableOpacity>
-        </View>
-        <View style={styles.contenido}>
-          <Text>Musica</Text>
-        </View>
-      </View>
+            
+          {/* <TouchableOpacity
+            style={styles.botonSubMenu}
+            onPress={() => {
+              setOrdenarNumeros(false);
+              setEncontrarNumeros(false);
+              setEscogerNumeros(true);
+
+            }}
+          >
+            <Text style={styles.txtSubmenu}>Escoge los pares</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.botonSubMenu}
+            onPress={() => {
+              setOrdenarNumeros(false);
+              setEscogerNumeros(false);
+              setEncontrarNumeros(true);
+            }}
+          >
+            <Text style={styles.txtSubmenu}>Encuentra los números</Text>
+          </TouchableOpacity> */}
+            
+            
+          </View>
+          {
+            escogerObjetos && <EscogerInstrumento setEscogerObjetos={setEscogerObjetos}/>
+          }
+          {
+            escogerNumeros && <Text>Escoge los pares</Text>
+          }
+          {
+            encontrarNumeros && <Text>Encuentra los números</Text>
+          }
+      </ImageBackground>
+    </View>
     );
   };
   
@@ -46,7 +109,7 @@ import {
   let { height, width } = Dimensions.get("screen");
   
   const styles = StyleSheet.create({
-    containerMusica: {
+    containerColores: {
       position: "absolute",
       top: 0,
       height: height < 500 ? height - 50 : width - 50,
@@ -63,13 +126,13 @@ import {
       // backgroundColor: "blue",
       width: "100%",
       alignItems: "center",
-      justifyContent: "space-between",
+      justifyContent: "flex-end",
       paddingHorizontal: 10,
       height: 50,
     },
     opcionesModulo: {
       display: "flex",
-      flexDirection: "row",
+      // flexDirection: "row",
       justifyContent: "center",
       alignItems: "center",
       gap: 20,
@@ -78,6 +141,18 @@ import {
       // position: "absolute",
       // top: 20,
       // right: 20,
+    },
+    botonSubMenu: {
+      backgroundColor: "#7986cb",
+      padding: 10,
+      borderRadius: 4,
+      width: 250,
+  
+    },
+    txtSubmenu: {
+      color: "#fff",
+      textAlign: "center"
+  
     },
     contenido: {
       display: "flex",
